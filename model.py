7b914026120e8c5157b886a728699385950b5168
Xinyu Li <72299922+Gariscat@users.noreply.github.com>
2024-07-30 17:09:05 +0800

    Update model.py

--- model.py
+++ model.py
@@ -5,13 +5,17 @@ from torchvision.models import \
     vit_b_16, ViT_B_16_Weights, \
     resnet152, ResNet152_Weights, \
     densenet201, DenseNet201_Weights, \
-    resnext101_32x8d, ResNeXt101_32X8D_Weights
+    resnext101_32x8d, ResNeXt101_32X8D_Weights, \
+    resnet18, ResNet18_Weights, \
+    resnet34, ResNet34_Weights, \
+    densenet121, DenseNet121_Weights
 from torch import nn, optim
 import torch.nn.functional as F
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
-from config import ALL_GENRES, HOP_FRAME
+from config import ALL_GENRES, HOP_FRAME, N_MELS
 from easydict import EasyDict as edict
 import math
+import torchmetrics
 
 
 class PositionalEncoding(nn.Module):
@@ -53,6 +57,14 @@ class HouseXModel(L.LightningModule):
             backbone = densenet201(weights=DenseNet201_Weights.DEFAULT)
         elif self.config.extractor_name == 'resnext101_32x8d':
             backbone = resnext101_32x8d(weights=ResNeXt101_32X8D_Weights.DEFAULT)
+        
+        elif self.config.extractor_name == 'resnet18':
+            backbone = resnet18(weights=ResNet18_Weights.DEFAULT)
+        elif self.config.extractor_name == 'resnet34':
+            backbone = resnet34(weights=ResNet34_Weights.DEFAULT)
+        elif self.config.extractor_name == 'densenet121':
+            backbone = densenet121(weights=DenseNet121_Weights.DEFAULT)
+        
         else:
             raise NotImplementedError(f"Extractor {self.config.extractor_name} is not supported.")
         
@@ -64,20 +76,23 @@ class HouseXModel(L.LightningModule):
         )
         self.positional_encoding = PositionalEncoding(self.config.d_model)
         self.encoder = TransformerEncoder(
-            TransformerEncoderLayer(d_model=self.config.d_model, nhead=12),
+            TransformerEncoderLayer(d_model=self.config.d_model, nhead=self.config.n_head),
             num_layers=model_config.transformer_num_layers
         )
         self.fc = nn.Linear(self.config.d_model, len(ALL_GENRES))
         
+        self.validation_step_outputs = []
+        self.train_step_outputs = []
+        
     def forward(self, x):
         b, c, h, w = x.shape
-        assert h == 224
+        assert h == N_MELS
         
         features = []
-        for i_frame in range(0, w-224, HOP_FRAME):
-            cur_patch = x[:, :, :, i_frame:i_frame+224]
-            if cur_patch.shape[-1] != 224:
-                zero_paddings = torch.zeros(b, c, h, cur_patch.shape[-1]-224)
+        for i_frame in range(0, w-N_MELS, HOP_FRAME):
+            cur_patch = x[:, :, :, i_frame:i_frame+N_MELS]
+            if cur_patch.shape[-1] != N_MELS:
+                zero_paddings = torch.zeros(b, c, h, cur_patch.shape[-1]-N_MELS)
                 cur_patch = torch.cat((cur_patch, zero_paddings), dim=-1)
             y = self.extractor(cur_patch)
             features.append(y)
@@ -96,6 +111,9 @@ class HouseXModel(L.LightningModule):
             self.config.loss_weight = self.config.loss_weight.to(x.device)
         loss = nn.CrossEntropyLoss(weight=self.config.loss_weight)(y_hat, y)
         self.log("train_loss", loss)
+        
+        self.train_step_outputs.append({'pred': y_hat.argmax(-1), 'label':y.argmax(-1)})
+        
         return loss
     
     def validation_step(self, batch, batch_idx):
@@ -105,7 +123,24 @@ class HouseXModel(L.LightningModule):
             self.config.loss_weight = self.config.loss_weight.to(x.device)
         loss = nn.CrossEntropyLoss(weight=self.config.loss_weight)(y_hat, y)
         self.log("val_loss", loss)
+        
+        self.validation_step_outputs.append({'pred': y_hat.argmax(-1), 'label':y.argmax(-1)})
+        
         return loss
+    
+    def on_train_epoch_end(self):
+        all_preds = torch.cat([_['pred'] for _ in self.train_step_outputs], dim=0)
+        all_labels = torch.cat([_['label'] for _ in self.train_step_outputs], dim=0)
+        acc = torchmetrics.Accuracy(task="multiclass", num_classes=8)(all_preds, all_labels)
+        self.log("train_acc", acc)
+        self.train_step_outputs.clear()  # free memory
+    
+    def on_validation_epoch_end(self):
+        all_preds = torch.cat([_['pred'] for _ in self.validation_step_outputs], dim=0)
+        all_labels = torch.cat([_['label'] for _ in self.validation_step_outputs], dim=0)
+        acc = torchmetrics.Accuracy(task="multiclass", num_classes=8)(all_preds, all_labels)
+        self.log("val_acc", acc)
+        self.validation_step_outputs.clear()  # free memory
     
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=self.config.learning_rate)
