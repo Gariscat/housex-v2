40abdc2240d008d49d108e02b23f036aa1ed650b
root <xl3133@nyu.edu>
2024-07-30 19:00:57 +0800

    sharpen-acc-ok

--- model.py
+++ model.py
@@ -83,6 +83,7 @@ class HouseXModel(L.LightningModule):
         
         self.validation_step_outputs = []
         self.train_step_outputs = []
+        self.metric = torchmetrics.Accuracy(task="multiclass", num_classes=len(ALL_GENRES))
         
     def forward(self, x):
         b, c, h, w = x.shape
@@ -131,15 +132,19 @@ class HouseXModel(L.LightningModule):
     def on_train_epoch_end(self):
         all_preds = torch.cat([_['pred'] for _ in self.train_step_outputs], dim=0)
         all_labels = torch.cat([_['label'] for _ in self.train_step_outputs], dim=0)
-        acc = torchmetrics.Accuracy(task="multiclass", num_classes=len(ALL_GENRES))(all_preds, all_labels)
+        self.metric = self.metric.to(all_preds.device)
+        acc = self.metric(all_preds, all_labels).item()
         self.log("train_acc", acc)
+        print('train_acc', acc)
         self.train_step_outputs.clear()  # free memory
     
     def on_validation_epoch_end(self):
         all_preds = torch.cat([_['pred'] for _ in self.validation_step_outputs], dim=0)
         all_labels = torch.cat([_['label'] for _ in self.validation_step_outputs], dim=0)
-        acc = torchmetrics.Accuracy(task="multiclass", num_classes=len(ALL_GENRES))(all_preds, all_labels)
+        self.metric = self.metric.to(all_preds.device)
+        acc = self.metric(all_preds, all_labels).item()
         self.log("val_acc", acc)
+        print('val_acc', acc)
         self.validation_step_outputs.clear()  # free memory
     
     def configure_optimizers(self):
